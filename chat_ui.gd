02ff4544e3c3cca8a98a249a1bf9ea5461be3b68
everton <40394108+TarikPonciano@users.noreply.github.com>
2025-01-30 14:19:36 -0300

    Criado Servidor

--- chat_ui.gd
+++ chat_ui.gd
@@ -1,5 +1,6 @@
 extends Control
 
+const PORT = 3333
 
 func _on_botao_enviar_pressed() -> void:
 	var mensagem = $CampoMensagem.text
@@ -14,3 +15,10 @@ func _on_botao_enviar_pressed() -> void:
 		$CampoMensagem.placeholder_text = "Digite sua mensagem..." 
 	else:
 		$CampoMensagem.placeholder_text = "VOCÊ NÃO PODE ENVIAR TEXTO VAZIO! ESCREVA ALGO!" 
+
+
+func _on_botao_host_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+	$CaixaMensagens.text += "Servidor criado na porta "+str(PORT)
